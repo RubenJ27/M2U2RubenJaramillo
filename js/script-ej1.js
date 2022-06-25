60c8acd20e41c64c4c4fd9c925e0bc7719542329
Ruben-Jaramillo <tiburones.tk@gmail.com>
2022-06-25 15:14:08 -0500

    Se hizo la estructura html del ejercicio numero 2 sin estilos

---
+++ js/script-ej1.js
@@ -0,0 +1,9 @@
+/*Ejercicio 1
+Crear un algoritmo que le permita al usuario ingresar:
+a. Nombre de un estudiante,
+b. Cantidad de materias o asignaturas a matricular.
+c. Valor de cada materia o asignatura,
+d. Valor de la papelería: que tiene un costo fijo de $20.000 y el carné con valor de $8.000.
+e. Descuento del 20% sobre el costo de las materias.
+Por medio del algoritmo imprimir el valor total de las materias que matriculó el estudiante con el
+descuento del 20% más los costos fijos.*/
---
+++ js/script-ej2.js
@@ -0,0 +1,6 @@
+/* Desarrolle un algoritmo que permita:
+f. Leer cuatro valores numéricos y almacenarlos en las variables A, B, C y D
+respectivamente.
+g. El algoritmo debe imprimir cual es el mayor valor y cual es el menor valor. Recuerde
+verificar que los cuatro valores introducidos por el teclado sean valores distintos.
+h. Crear un mensaje de alerta en caso que se detecte la introducción de valores iguales. */
