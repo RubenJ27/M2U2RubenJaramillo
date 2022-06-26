25066c268f0c79e02535663024f9857435904b7e
Ruben-Jaramillo <tiburones.tk@gmail.com>
2022-06-26 16:53:22 -0500

    Se crearon las variables para almacenar los valores en js del ejercicio 2

--- js/script-ej2.js
+++ js/script-ej2.js
@@ -3,4 +3,19 @@ f. Leer cuatro valores numéricos y almacenarlos en las variables A, B, C y D
 respectivamente.
 g. El algoritmo debe imprimir cual es el mayor valor y cual es el menor valor. Recuerde
 verificar que los cuatro valores introducidos por el teclado sean valores distintos.
-h. Crear un mensaje de alerta en caso que se detecte la introducción de valores iguales. */
+h. Crear un mensaje de alerta en caso que se detecte la introducción de valores iguales.*/
+
+
+
+let valueOne = document.getElementById("numOne");
+let valueTwo = document.getElementById("numTwo");
+let valueThree = document.getElementById("numThree");
+let valueFour = document.getElementById("numFour");
+
+let valueBtn = document.getElementById("btn__validator");
+let valueBtnclean = document.getElementById("btn__clean");
+
+
+
+
+
