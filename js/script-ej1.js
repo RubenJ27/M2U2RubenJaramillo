aa640078941af2e95748cd65e986aee22499ef1a
Ruben-Jaramillo <tiburones.tk@gmail.com>
2022-06-27 14:10:39 -0500

    Se creo el archivo con las variables y declaraciones base en js del ejercicio 1

--- js/script-ej1.js
+++ js/script-ej1.js
@@ -7,3 +7,30 @@ d. Valor de la papelería: que tiene un costo fijo de $20.000 y el carné con va
 e. Descuento del 20% sobre el costo de las materias.
 Por medio del algoritmo imprimir el valor total de las materias que matriculó el estudiante con el
 descuento del 20% más los costos fijos.*/
+
+let $form = document.getElementById("container__validator__form");
+let $nomEst = document.getElementById("nomEst");
+let $asigMat = document.getElementById("asigMat");
+
+let $btnValidator = document.getElementById("btn__validator");
+
+let $btnClean = document.getElementById("btn__clean");
+
+
+document.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let nomEstvalue = $nomEst.value;
+  let asigMatvalue = parseInt($asigMat.value);
+
+  if (e.target === $form) {
+    console.log(A)
+  }
+});
+
+document.addEventListener("click", (e) => {
+  if (e.target === $form.cerrar) {
+    $valueAone.value = "";
+    $valueBtwo.value = "";
+  }
+});
